5ba9faad910c9345c6ab052bfbad629f12991f10
SELAO <steven.selao@etu.unistra.fr>
2026-02-10 12:26:23 +0100

    Classe Tower / Enemy (surtout les stats)

---
+++ Enemy.cpp
@@ -0,0 +1,9 @@
+class Enemy{
+private:
+    // Stats de chaque ennemies
+    double lp; // Life Point
+    double speed; // Speed
+    double resistance; // Resistance
+    bool fly; // Fly
+   
+}
---
+++ Tower.cpp
@@ -0,0 +1,11 @@
+class Tower{
+private:
+    // Stats de chaque tours
+    double range; // Range
+    double damage; // Damage
+    double as; // Attack Speed 
+    double rs; // Rotation Speed
+    double ps; // Projectile Speed
+
+    
+}
