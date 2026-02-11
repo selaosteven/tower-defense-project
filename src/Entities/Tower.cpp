4b3bfaee46efde3ec04daf2440649c59c576f3b4
SELAO <steven.selao@etu.unistra.fr>
2026-02-11 10:22:03 +0100

    Unique_ptr Tower

--- src/Entities/Tower.cpp
+++ src/Entities/Tower.cpp
@@ -1,11 +1,10 @@
 #include "Tower.h"
-#include <iostream>
 
 int Tower::compteur = 0;
 
 // Constructeur
 Tower::Tower(double range, double damage, double as, double rs, Projectile proj, std::string type) : 
-range(range), damage(damage), as(as), rs(rs), ps(ps) ,id(compteur++) {} 
+range(range), damage(damage), as(as), rs(rs),type(type),id(compteur++) {} 
 
 int Tower::getId() const {
     return id;
--- src/Entities/Tower.h
+++ src/Entities/Tower.h
@@ -1,17 +1,21 @@
 #ifndef TOWER_H
 #define TOWER_H
 
-#include <Projectile.h>
-#include <string.h>
+#include "Projectile.h"
+#include <string>
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
 
-class Tower{
+
+class Tower {
 protected:
     // Stats de chaque tours
     double range; // Range
     double damage; // Damage
     double as; // Attack Speed 
     double rs; // Rotation Speed
-    Projectile proj; // Classe Projectile 
+    std::unique_ptr<Projectile> proj; // Classe Projectile 
     std::string type; // Type de la tour
     int id; 
     static int compteur;
@@ -19,7 +23,6 @@ protected:
 public:
     Tower(double range, double damage, double as, double rs, Projectile proj, std::string type); // Constructeur
     int getId() const; // Getter ID
-
 };
 
 #endif
