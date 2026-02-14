4af1b110627205e430c91af445d3384269cd34fc
Victor GIERLOWSKI <victor.gierlowski@gmail.com>
2026-02-14 18:12:50 +0100

    ✨ Class Window Multi fenêtre
    Implémentation de la classe window qui permet de gérer des fenêtres SDL.
    Très simple, pas encore d'affichage à l'intérieur mais complète encapsulation de la SDL.
    Peut ouvrir plusieurs fenêtres et partager les events grâce à un mutex.
    
    La pipeline d'affichage à l'intérieur est à ajouter.
    
    ```cpp
    #include <iostream>
    #include <chrono>
    #include <thread>
    #include "UI/Window.h"
    
    int main(int argc, char *argv[]){
        UI::Window * mainFenetre = new UI::Window{};
        std::this_thread::sleep_for(std::chrono::milliseconds(200));
        UI::Window * anotherFenetre = new UI::Window{};
        while(UI::Window::isThereAnInstance()){
            std::this_thread::sleep_for(std::chrono::milliseconds(200));
        }
        return 0;
    }
    ```

---
+++ src/UI/Window.h
@@ -0,0 +1,67 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#include <SDL.h>
+#include <mutex>
+#include <string>
+
+namespace UI
+{
+
+void print_sdl_error(const char * msg);
+
+const Uint32 auto_flags_sdl_init = SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS;
+const Uint32 auto_flags_sdl_window = SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE;
+const Uint32 auto_flags_sdl_renderer = SDL_RENDERER_ACCELERATED;
+
+enum werrors {
+    NONE=0,
+    SDL_ALREADY_INITIATED=-1,
+    SDL_INIT_FAILED,
+    STOP,
+};
+using werrors = enum werrors;
+
+class Window
+{
+
+
+// Static - classwide
+private:
+    static bool sdl_initiated;
+    static unsigned int number_of_instances;
+    static Uint32 sdl_flags;
+    static int instanceWindowThread(void * window);
+    static std::mutex event_mutex;
+public: 
+    static werrors init_sdl();
+    static werrors init_sdl(Uint32 flags);
+    static bool isThereAnInstance();
+
+// ----------------------------
+// Object elements
+private:
+    SDL_Renderer * renderer_;
+    SDL_Window * window_;
+    SDL_Event * event_;
+    Uint64 ticks_;
+    
+
+protected:
+    int win_width_;
+    int win_height_;
+    const Uint32 win_flags_;
+    float delta_time_;
+
+public:
+    Window();
+    Window(int width, int height);
+    Window(int width, int heightn, Uint32 flags);
+    int Create(void * args);
+    werrors inputs();
+    void loop();
+    ~Window();
+
+};
+
+}
+#endif
