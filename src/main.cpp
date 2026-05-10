5a7dfff1f9be96356803b0bcbc603669d0b93a06
vgierlowski <victor.gierlowski@etu.unistra.fr>
2026-05-10 20:50:49 +0200

    move files

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@
 
 // Choisira si on lance la session ou le mode editeur
 int main(int argc, char *argv[]){
-    if(argc > 1){
-        UI::Session test{argv[1]};
-        test.mainSession();
-        test.waitForClose();
-    }
+    std::string map = (argc > 1) ? argv[1] : "../src/Ressources/map.txt";
+    UI::Session test{map};
+    test.mainSession();
+    test.waitForClose();
+    
     return 0;
 }
